e9bfc51aaafd9cc48af93041c61d55adbf830be1
yaotzin1 <Piotr.Solarz-Wnek@rec-global.com>
2016-03-02 11:12:42 +0100

    Some changes according to #atb-3

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
 var gulpWatch = require('gulp-watch');
 var gulpJspm = require('gulp-jspm');
+var del = require('del');
 
 gulp.task('inject', function(){
     var target = gulp.src('./app_ts/index.html');
@@ -41,17 +42,51 @@ gulp.task('typescript',function(){
     return tsResult.js.pipe(sourcemaps.write()).pipe(gulp.dest('./app'));
 });
 
-gulp.task('copy', function(){
+gulp.task('gulp-watch', function(){
+    gulp.src('./app_ts/**/*.html', {base: './app_ts'})
+    .pipe(gulpWatch('./app_ts', {base: './app_ts'}))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('annotate-angular', function(){
+    return gulp.src(['./**/*.js','!./**/*.js.map', '!./libs/**/*.js'], {cwd:'./app/'})
+    .pipe(ngAnnotate())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('buildDistWrapper', function(){
+    return gulp.src('./app/app.bootstrap.js')
+    .pipe(gulpJspm({
+        minify: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('sass', function () {
+    return gulp.src('./app_ts/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./app'));
+});
+
+//watch
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./app_ts/**/*.scss', ['sass']);
+});
+
+//Developement tasks
+
+gulp.task('serve', ['gulp-watch'],function(){
+    runSequence('build:dev', 'run-server:dev');
+});
+
+gulp.task('copy:dev', function(){
     return gulp
             .src(['./app_ts/**/*.html']).pipe(gulp.dest('./app'));
 });
 
-gulp.task('copy:dist', function(){
-    return gulp
-            .src(['./app/**/*.html', './app/**/*.css', './app/libs/*.js']).pipe(gulp.dest('./dist'));
-});
-
-gulp.task('run-server', function(){
+gulp.task('run-server:dev', function(){
     return gulp.src('./app')
     .pipe(server({
         livereload:true,
@@ -61,37 +96,20 @@ gulp.task('run-server', function(){
         port:9000,
         open:true
     }));
-})
-
-gulp.task('gulp-watch', function(){
-    gulp.src('./app_ts/**/*.html', {base: './app_ts'})
-    .pipe(gulpWatch('./app_ts', {base: './app_ts'}))
-    .pipe(gulp.dest('./app'));
 });
 
-gulp.task('annotate-angular', function(){
-    return gulp.src(['./**/*.js','!./**/*.js.map', '!./libs/**/*.js'], {cwd:'./app/'})
-           .pipe(ngAnnotate())
-           .pipe(gulp.dest('dist'));
+gulp.task('build:dev', function(){
+    runSequence(['typescript', 'libs', 'sass'],'copy:dev','inject');
 });
 
-gulp.task('buildDistWrapper', function(){
-    return gulp.src('./app/app.bootstrap.js')
-       .pipe(gulpJspm({
-           minify: true
-       }))
-       .pipe(gulp.dest('./dist'));
+//DISTRIBUTE BUILD
+
+gulp.task('copy:dist', function(){
+    return gulp
+            .src(['./app/**/*.html', './app/**/*.css', './app/libs/*.js']).pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build', function(){
-    runSequence(['typescript', 'libs', 'sass'],'copy','inject');
-});
-
-gulp.task('build:dist', function(){
-    runSequence('buildDistWrapper', 'annotate-angular', 'copy:dist');
-});
-
-gulp.task('serve:dist', function(){
+gulp.task('run-server:dist', function(){
     return gulp.src('./dist')
     .pipe(server({
         livereload:true,
@@ -103,16 +121,9 @@ gulp.task('serve:dist', function(){
     }));
 });
 
-gulp.task('serve', ['gulp-watch'],function(){
-    runSequence('build', 'run-server');
+gulp.task('build:dist', function(){
+    runSequence('buildDistWrapper', 'annotate-angular', 'copy:dist');
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./app_ts/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./app_ts/**/*.scss', ['sass']);
+gulp.task('serve:dist', ['run-server:dist'],function(){
 });
